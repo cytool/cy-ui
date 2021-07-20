60563c86bc077d2a72e9e3fbfcd65715837bffed
linjie <632220228@qq.com>
2021-07-20 09:29:17 +0800

    fix: 修复touch事件，修改类名，避免冲突

--- src/js/components/actionSheet.js
+++ src/js/components/actionSheet.js
@@ -1,12 +1,14 @@
 var cyui = {}
 
-cyui.action = function (args) {
+cyui._Action = function (args) {
     this.createDom()
     this.dom = document.querySelector(args.dom || args)
     this.show = false
     this.dom.querySelector('.cyui-action-row').addEventListener('click', () => this.showBox(true))
     document.querySelector('.cyui-action-change').addEventListener('click', () => this.showBox(false))
-    document.querySelector('.cancel').addEventListener('click', () => this.showBox(false))
+    document.querySelector('.cyui-action-cancel').addEventListener('click', () => this.showBox(false))
+    document.querySelector('.cyui-action-cancel').addEventListener('touchstart', e => this.touchStart(e))
+    document.querySelector('.cyui-action-cancel').addEventListener('touchend', e => this.touchEnd(e))
     document.querySelector('.cyui-action-dialog').addEventListener('click', () => this.showBox(false))
     let domArr = document.querySelectorAll('.cyui-action-changeBox')
     console.log(domArr, "------DOMARR--------")
@@ -14,11 +16,11 @@ cyui.action = function (args) {
     for (let o of domArr) {
         console.log(o, "---------OOOOOOOO")
         o.addEventListener('touchstart', e => this.touchStart(e))
-        o.addEventListener('touchmove', e => this.touchMove(e))
+        o.addEventListener('touchend', e => this.touchEnd(e))
     }
 }
 
-cyui.action.prototype = {
+cyui._Action.prototype = {
     /**
      * 创建插件所需节点
      * @param {Boolean} show 是否显示弹窗
@@ -39,7 +41,7 @@ cyui.action.prototype = {
                     </div>
                 </div>
                 <div class="cyui-action-bg"></div>
-                <div class="cyui-action-change cancel">取消</div>
+                <div class="cyui-action-cancel">取消</div>
             </div>
             `
             document.querySelector('body').appendChild(contentNode)
@@ -54,12 +56,12 @@ cyui.action.prototype = {
         this.show = show
         if (this.show) {
             document.querySelector('.cyui-action-dialog').classList.add('on')
-            document.querySelector('.cyui-action-change').classList.remove('changeOn')
             setTimeout(() => {
                 document.querySelector('.cyui-action-box').classList.add('on')
             })
         } else {
             document.querySelector('.cyui-action-box').classList.remove('on')
+            // document.querySelector('.cyui-action-cancel').classList.add('on')
             setTimeout(() => {
                 document.querySelector('.cyui-action-dialog').classList.remove('on')
             }, 500)
@@ -70,7 +72,14 @@ cyui.action.prototype = {
      * @return {void}
      */
     touchStart(evt) {
-        console.log(evt, "鼠标点击------------")
-        document.querySelector('.cyui-action-change').classList.add('changeOn')
+        console.log(evt, "鼠标点击开始------------")
+        var c = evt.changedTouches[0].target
+        console.log(c, "--------ccccccccc")
+        // c.appendChild('changeOn');
+        c.style.backgroundColor = "#f2f3f5"
+    },
+    touchEnd(evt) {
+        var c = evt.changedTouches[0].target
+        c.style.backgroundColor = "#fff"
     },
 }
